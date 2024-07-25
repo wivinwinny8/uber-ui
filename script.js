a57fc8669f8fcf4cdd9292cedef7b4fb77516eec
wivinwinny8 <wivinwinnyv@gmail.com>
2024-07-25 23:19:37 +0530

    -m

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ function dropHide(){
 }
 
 
+
+const btnScrollToTop = document.querySelector(".scrollup");
+
+// scroll to top of page when button clicked
+btnScrollToTop.addEventListener("click", e => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
+
+// toggle 'scroll to top' based on scroll position
+window.addEventListener('scroll', e => {
+  btnScrollToTop.style.display = window.scrollY > 500 ? 'block' : 'none';
+});
